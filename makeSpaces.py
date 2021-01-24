fe7486315bf07704bf11573818dbbd232fa7e0b7
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-01-24 03:06:19 +0300

    Add files via upload
    Some new cashiers started to work at your restaurant.
    
    They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
    
    All the orders they create look something like this:
    
    "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
    
    The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
    
    Their preference is to get the orders as a nice clean string with spaces and capitals like so:
    
    "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
    
    The kitchen staff expect the items to be in the same order as they appear in the menu.
    
    The menu items are fairly simple, there is no overlap in the names of the items:
    
    1. Burger
    2. Fries
    3. Chicken
    4. Pizza
    5. Sandwich
    6. Onionrings
    7. Milkshake
    8. Coke

---
+++ makeSpaces.py
@@ -0,0 +1,13 @@
+def get_order(order):
+    lt = "Burger Fries Chicken Pizza Sandwich Onionrings Milkshake Coke".split() 
+    res = ""
+    for item in lt:
+        if item.casefold() in order:
+            for i in range(0,order.count(item.casefold())):
+                res += item + " "
+    return res[:-1]
+
+test = "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+
+print(get_order(test))
+
