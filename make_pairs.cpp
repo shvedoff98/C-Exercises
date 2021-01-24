df6957aa987551749fcc3c977b43802fb4f371a0
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-01-25 00:02:02 +0300

    Add files via upload
    The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
    
    The result array should be sorted in ascending order of values.
    
    Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
    
    [1, 2, 3, 4]  should return [[1, 3], [2, 4]]
    
    [4, 1, 2, 3]  should also return [[1, 3], [2, 4]]
    
    [1, 23, 3, 4, 7] should return [[1, 3]]
    
    [4, 3, 1, 5, 6] should return [[1, 3], [3, 5], [4, 6]]

---
+++ make_pairs.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+
+void function(const vector<int> &vec){
+	vector<pair<int,int>>result;
+	vector<int> sorted_vec;
+	sorted_vec = vec;
+	sort(sorted_vec.begin(),sorted_vec.end());
+	for(int i=0;i<sorted_vec.size();i++){
+		for(int j=0;j<sorted_vec.size();++j){
+			if(sorted_vec[i]+2==sorted_vec[j]){
+				pair<int,int> p;
+				p = make_pair(sorted_vec[i],sorted_vec[j]);
+				result.push_back(p);
+			}
+		}
+	}
+	for(int i=0;i<result.size();++i){
+		cout<<result[i].first<<result[i].second<<" ";
+	}
+	cout<<endl;
+}
+
+
+int main(){
+	vector<pair<int,int>> function(const vector<int> x);
+	return 0;
+}
