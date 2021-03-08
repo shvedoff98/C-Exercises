8c5c826c174d6f16e882090b2bbde6d1615d5f54
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-08 07:53:02 +0300

    Add files via upload
    Your task, is to create NxN multiplication table, of size provided in parameter.
    
    for example, when given size is 3:
    
    1 2 3
    2 4 6
    3 6 9
    
    for given example, the return value should be: [[1,2,3],[2,4,6],[3,6,9]]

---
+++ Multiplication_table.cpp
@@ -0,0 +1,50 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+vector <vector<int>> multiplication_table(int n)
+{
+    int count = 2;
+    vector <vector<int>> result;
+    vector <int> temp1;
+    vector <int> temp;
+    for(int j = 1; j < n+1; ++j)
+    {
+        temp1.push_back(j);
+    }
+    result.push_back(temp1);
+
+    for(int m = 1; m < n; ++m)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            temp.push_back(temp1[i]*count);
+        }
+        result.push_back(temp);
+        temp.clear();
+        count ++;
+
+    }
+
+
+    for (const auto it : result)
+    {
+        for(const auto jt : it)
+        {
+            cout << "  " << jt << "  ";
+        }
+        
+        cout << "\n";
+    }
+
+    return result;
+}
+
+int main()
+{
+    int test = 5;
+    multiplication_table(test);
+    return 0;
+}
