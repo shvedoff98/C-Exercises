0c6e84800d8b16d872bfee603b13526ff57039cf
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-12 13:22:57 +0300

    Add files via upload
    Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
    
    A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
    
    Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
    
        your referral bonus, and
    
        the price of a beer can
    
    For example:
    
    beeramid(1500, 2); // should === 12
    beeramid(5000, 3); // should === 16

---
+++ Beeramid.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+int beeramid(int bonus , double price)
+{
+    if ( bonus < price ) {return 0;}
+    
+    int sum = 1, temp, increment = 2;
+    auto f = bonus / price;
+    
+    //convert double to int if:
+    if (abs(f - int(f)) > 0){f = static_cast<int>(f);}
+    if (f > 0)
+    {
+        while ( sum < f )
+        {
+            sum += pow(increment,2);
+            if (sum > f) {return increment-1;}
+            increment ++;
+        }
+    }
+    return increment-1;
+}
+
+int main(int argc, char* argv[])
+{
+
+    beeramid(-1,4);
+    return 0;
+}
