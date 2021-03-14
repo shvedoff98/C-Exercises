2b36c8a169273af92dbc9f89375643db1893f39a
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-14 14:47:31 +0300

    Add files via upload
    Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
    
    Notes:
    
        Only lower case letters will be used (a-z). No punctuation or digits will be included.
        Performance needs to be considered
    
    Input strings s1 and s2 are null terminated.
    
    Examples
    
    scramble('rkqodlw', 'world') ==> True
    scramble('cedewaraaossoqqyt', 'codewars') ==> True
    scramble('katas', 'steak') ==> False

---
+++ Scramblies.cpp
@@ -0,0 +1,29 @@
+#include <cstring>
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool scramble(const std::string& s1, const std::string& s2)
+{
+    string copy = s1;
+    int sum = 0;
+    for (int i=0; i < s2.size(); ++i)
+    {
+        size_t found = copy.find(string(1,s2[i]));
+        if (found != string::npos)
+        {
+            sum++;
+            copy.erase(found,1);
+            continue;
+        }
+    }
+    if (sum == s2.size()){return true;}
+    return false;
+
+}
+
+int main(int argc, char* argv[])
+{
+    scramble("sammoc", "commas");
+    return 0;
+}
