bb45fb3160bc72554b9d7c578e2ca0699a924fce
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-18 13:10:08 +0300

    Add files via upload
    Description:
    
    Write a program to determine if a string contains only unique characters. Return true if it does and false otherwise.
    
    The string may contain any of the 128 ASCII characters. Characters are case-sensitive, e.g. 'a' and 'A' are considered different characters.

---
+++ All_unique.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool hashUniqueChars(string s)
+{
+    if (s.size()==0) return false;
+    for (int i=0; i<s.size(); ++i)
+    {
+        size_t found = s.rfind( string(1,s[i]) );
+        if (found != string::npos && found != i){ return false; }
+    }
+    return true;
+}
+int main(int argc,char* argv[])
+{
+    cout << hashUniqueChars("") << "\n";
+    return 0;
+}
