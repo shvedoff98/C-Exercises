cb97e640177e7226a1f0ffdce70dc4999c2ce595
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-18 13:22:51 +0300

    Update All_unique_optimized.cpp

--- All_unique_optimized.cpp
+++ All_unique_optimized.cpp
@@ -1,20 +1,12 @@
 #include <set>
-#include <iostream>
 using namespace std;
 
 bool hasUniqueChars(string s)
 {
-    int len = s.size();
-    for (set<char>::iterator it=myset.begin(); it!= myset.end(); ++it)
-    {
-        cout << " " << *it;
-    }
-    cout << "\n";
-
     return s.size() == set<char>(s.begin(),s.end()).size();
 }
 
 int main()
 {
-    cout << hasUniqueChars("abcdef") << "\n";
+    hasUniqueChars("abcdef") << "\n";
 }
