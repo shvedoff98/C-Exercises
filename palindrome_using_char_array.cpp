396a23ed30ae046102b2ad44890990c38e95ade1
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-19 17:16:29 +0300

    Add files via upload

---
+++ palindrome_using_char_array.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+bool is_palindrome(const char s[], int n)
+{
+    int first = 0;
+    int last = n-1;
+    while (first < last)
+    {
+        if (s[first]!=s[last]) return false;
+        ++first;
+        --last;
+    }
+
+    return true;
+}
+
+
+istream& read_word(istream& is, char* buffer, int max)
+    // reads  >= max-1 elements in char array.
+{
+    is.width(max);
+    is >> buffer;
+    return is;
+}
+
+int main()
+{
+    constexpr int max = 128;
+    for (char s[max]; read_word(cin,s,max);)
+    {
+        cout << s << " is";
+        if(!is_palindrome(s,strlen(s))) cout << " not";
+        cout << " a palindrome.\n";
+    }
+}
