9b670842e2766329c0dda16f0fdef1f1867576fc
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-03-29 16:17:34 +0300

    Add files via upload

---
+++ to_roman.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <utility>
+#include <iostream>
+using namespace std;
+
+string to_roman(signed long long num)
+{
+    string result;
+    vector<pair<unsigned int, const char*>>roman {
+        {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+        {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+        {10 , "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+    };
+
+    for (auto &x : roman)
+    {
+        while ( num >= x.first)
+        {
+            result += x.second;
+            num -= x.first;
+        }
+    }
+    return result;
+
+}
+
+int main(int argc, char* agrv[])
+{
+    for (int i=1; i<=100; ++i)
+    {
+        cout << to_roman(i) << " ";
+    }
+    cout << "\n";
+}
