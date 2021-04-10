deb5baa6df7077445e1c86a431e4fe209bbc5e41
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-04-10 09:35:33 +0300

    Add files via upload
    Description:
    
    Trolls are attacking your comment section!
    
    A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
    
    Your task is to write a function that takes a string and return a new string with all vowels removed.
    
    For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
    
    Note: for this kata y isn't considered a vowel.

---
+++ Disemvowel_Trolls.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+string disemvowel(string str)
+{
+    string res;
+    for (auto& x : str) {
+        if ( x == 'a' || x == 'A') { continue; }
+        if ( x == 'i' || x == 'I') { continue; }
+        if ( x == 'o' || x == 'O') { continue; }
+        if ( x == 'e' || x == 'E') { continue; }
+        if ( x == 'u' || x == 'U') { continue; }
+        res += x;
+    }
+    return res;
+}
+
+int main(int argc, char* argv[])
+{
+    string str {"This website is for losers LOL!"};
+    cout << disemvowel(str) << "\n";
+}
