3db9e33023da0a51035845382836f6abc27fb6f3
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-04-11 06:31:15 +0300

    Add files via upload
    Description:
    
    You and a group of friends are earning some extra money in the school holidays by re-painting the numbers on people's letterboxes for a small fee.
    
    Since there are 10 of you in the group each person just concentrates on painting one digit! For example, somebody will paint only the 1's, somebody else will paint only the 2's and so on...
    
    But at the end of the day you realise not everybody did the same amount of work.
    
    To avoid any fights you need to distribute the money fairly. That's where this Kata comes in.
    Kata Task
    
    Given the start and end letterbox numbers, write a method to return the frequency of all 10 digits painted.
    Example
    
    For start = 125, and end = 132
    
    The letterboxes are
    
        125 = 1, 2, 5
        126 = 1, 2, 6
        127 = 1, 2, 7
        128 = 1, 2, 8
        129 = 1, 2, 9
        130 = 1, 3, 0
        131 = 1, 3, 1
        132 = 1, 3, 2
    
    The digit frequencies are:
    
        0 is painted 1 time
        1 is painted 9 times
        2 is painted 6 times
        etc...
    
    and so the method would return [1,9,6,3,0,1,1,1,1,1]
    Notes
    
        0 < start <= end
        In C, the returned value will be free'd.

---
+++ 7 kyu/Letterbox_Paint-Squad.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <array>
+using namespace std;
+
+int my_count(vector<int> vec, int num)
+{
+    int count = 0;
+    for (auto it = vec.begin(); it != vec.end(); ++it)
+    {
+        if ( *it == num ) ++ count;
+    }
+    return count;
+}
+
+array<int, 10> paint_letterboxes(int start, int end)
+{
+    array<int, 10> res; 
+    vector <int> digits;
+
+    for (int i=start; i <= end; ++i)
+    {
+        int temp = i;
+        do {
+            int num = temp % 10;
+            digits.push_back(num); 
+        } while ( temp /= 10 );
+    }
+
+    for (int i=0; i<10; ++i) { res[i] = my_count(digits, i); }
+
+    return res;
+}
+
+int main(int argc, char* argv[])
+{
+    int start, end;
+    cout << "Enter the start: "; cin >> start;
+    cout << "Enter the end: ";  cin >> end;
+    array <int, 10> res = paint_letterboxes(start, end);
+    for (const auto& x : res) { cout << x << " "; }; cout << "\n";
+}
