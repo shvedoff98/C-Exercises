0356b984e7cb223aa56e948546595043ad803d5a
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-04-12 07:21:20 +0300

    Add files via upload
    Description:
    
    Remember the triangle of balls in billiards? To build a classic triangle (5 levels) you need 15 balls. With 3 balls you can build a 2-level triangle, etc.
    
    For more examples,
    
    pyramid(1) == 1
    
    pyramid(3) == 2
    
    pyramid(6) == 3
    
    pyramid(10) == 4
    
    pyramid(15) == 5
    
    Write a function that takes number of balls (≥ 1) and calculates how many levels you can build a triangle.

---
+++ 7 kyu/Billiards_triangle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int pyramid(int n)
+{
+    int levels = 0;
+    int count = 1; int num = 1;
+    while (count <= n)
+    {
+        ++levels; ++num;
+        count += num;
+    }
+    return levels;
+}
+
+int main(int argc, char* argv[])
+{
+    cout << "Enter the number of balls: ";
+    int n; cin >> n;
+    cout << pyramid(n) << "\n";
+}
