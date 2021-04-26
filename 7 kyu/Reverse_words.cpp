6f3e79e2f36059509f279be0f1c8e5fb21ac4540
Kirill Shvedov <74286567+shvedoff98@users.noreply.github.com>
2021-04-26 21:14:01 +0300

    Add files via upload
    Description:
    
    Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
    Examples
    
    "This is an example!" ==> "sihT si na !elpmaxe"
    "double  spaces"      ==> "elbuod  secaps"

---
+++ 7 kyu/Reverse_words.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <iostream>
+#include <algorithm>
+
+template <typename T>
+void swap (T& a, T&b)
+{
+    T c {a}; a = b; b = c;
+}
+
+std::string reverse_str(std::string& str)
+{
+    int sz = str.size();
+    for (int i=0; i<sz/2; ++i)
+    {
+        swap(str[i], str[sz-i-1]);
+    }
+    return str;
+}
+
+std::string reverse_words(std::string str)
+{
+    std::string result, temp;
+    for (auto& x : str)
+    {
+        if (x == ' ')
+        {
+            result += reverse_str(temp);
+            result += ' ';
+            temp = "";
+            continue;
+        }
+        temp += x;
+    }
+    return result += reverse_str(temp);
+}
+
+int main(int argc, char* argv[])
+{
+    std::string str {"The quick brown fox jumps over the lazy dog."};
+    std::cout << reverse_words(str) << "\n";
+}
